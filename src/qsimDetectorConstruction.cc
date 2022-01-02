76227ea44315f9a669670633d89d985685fc1ead
dvalmassei <95585992+dvalmassei@users.noreply.github.com>
2022-01-01 20:34:22 -0500

    Update qsimDetectorConstruction.cc
    LAM attempt

--- src/qsimDetectorConstruction.cc
+++ src/qsimDetectorConstruction.cc
@@ -999,11 +999,11 @@ G4double Reflectivity_laterals[nEntries];// = {0.7612, 0.7621, 0.764, 0.764, 0.7
       rotlg->rotateY(0.*rad);
       rotlg->rotateZ(0.*rad);
 
-      rot_pmt->rotateY(M_PI/2.*rad);
+      rot_pmt->rotateY(0.*rad);
       //G4VPhysicalVolume* tmirror_phys = new G4PVPlacement(rot_pmt,G4ThreeVector(7.25*cm+lngth+2.*cm,0.,.9*cm),tmirror_log,"TMirror",det_log,false,0);
       //G4VPhysicalVolume* lightguide_phys = new G4PVPlacement(rotlg,G4ThreeVector(0.*cm,0,-0.375*cm+.9*cm),lightguide_log,"lightguide_phys", det_log,false,0);
-      G4VPhysicalVolume* pmt_phys = new G4PVPlacement(rot_pmt,G4ThreeVector(7.25*cm+2.*plngth+7.*cm,0.,.9*cm),pmt_log,"PMT",det_log,false,0);
-      G4VPhysicalVolume* cath_phys = new G4PVPlacement(rot_pmt,G4ThreeVector(7.25*cm+2.*plngth+7.*cm,0.,.9*cm),cath_log,"CATH",det_log,false,0);
+      G4VPhysicalVolume* pmt_phys = new G4PVPlacement(rot_pmt,G4ThreeVector(7.25*cm+2.*plngth+7.*cm,10.,.9*cm),pmt_log,"PMT",det_log,false,0);
+      G4VPhysicalVolume* cath_phys = new G4PVPlacement(rot_pmt,G4ThreeVector(7.25*cm+2.*plngth+7.*cm,10.,.9*cm),cath_log,"CATH",det_log,false,0);
 
       G4OpticalSurface* CTHOpSurface = new G4OpticalSurface("CathodeOpSurface");
       CTHOpSurface -> SetType(dielectric_metal);
