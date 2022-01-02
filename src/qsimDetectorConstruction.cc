b11203d075e014ed3c950b580ca759230ab9d76d
dvalmassei <95585992+dvalmassei@users.noreply.github.com>
2022-01-01 23:19:52 -0500

    Update qsimDetectorConstruction.cc

--- src/qsimDetectorConstruction.cc
+++ src/qsimDetectorConstruction.cc
@@ -491,7 +491,7 @@ G4double Reflectivity_laterals[nEntries];// = {0.7612, 0.7621, 0.764, 0.764, 0.7
       rm->rotateX(0.*deg);
 
       G4VSolid* quartz_log_half
-      = new G4UnionSolid("QuartzHalf", quartz_log, quartz_log2, rm, G4ThreeVector(18.*cm,12.*cm,0.*cm));
+      = new G4UnionSolid("QuartzHalf", quartz_box, quartz_box2, rm, G4ThreeVector(18.*cm,12.*cm,0.*cm));
 
     }
 
